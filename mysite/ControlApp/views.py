310b3ccd908b916706dafe118f7b326b38c531fa
AlcalinoGitHub <alcalinouwu69@gmail.com>
2023-02-12 15:24:40 -0600

    Added html functionality
    -Added static Folder
    -Added Main Page HTML/CSS
    -Added static folder to the settings
    -Replaced mainPage method to show the mainPage html

--- mysite/ControlApp/views.py
+++ mysite/ControlApp/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
 
 
 def MainPage(request):
-    return HttpResponse('MainPage')
+    return redirect('/static/MainPage.html')
