793fef4850b592853108131e4c659e118db4e658
AlcalinoGitHub <alcalinouwu69@gmail.com>
2023-02-12 19:43:17 -0600

    Added try/Except statement to avoid crash on register field
    -Added try except statements in the register Function on views.py on Accounts app to prevent the app from throwing an exception, and instead returning a message to the user

--- mysite/Accounts/views.py
+++ mysite/Accounts/views.py
@@ -9,19 +9,23 @@ def Empty(request):
 
 def register(request):
     if request.method == "POST":
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
         username = request.POST['username']
         email = request.POST['email']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
+
+
         if password1 == password2:
             if User.objects.filter(username = username).exists() or User.objects.filter(email=email).exists():
                 print('username Taken')
                 messages.info(request, 'Username or email already Taken')
                 return redirect('/accounts/register/')
             else:
-                user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
+                try:
+                    user = User.objects.create_user(username=username, password=password1, email=email)
+                except: 
+                    messages.info(request, 'Fields must not be empty')
+                    return redirect('/accounts/register/')
                 user.save();
                 messages.info(request,'user created')
                 return redirect('/accounts/login/')
