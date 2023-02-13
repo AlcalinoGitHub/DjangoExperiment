ba5d8fb803e62b0d2aa647e95eca0d5f39fd5d33
AlcalinoGitHub <alcalinouwu69@gmail.com>
2023-02-12 22:22:16 -0600

    Added Task app and Model
    -Created TaskApp (app)
    -Created Task model (linked to users model by forgein key)
    -Updated the database with makemigrations/migrate commands

---
+++ mysite/TaskApp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Task(models.Model):
+    Name = models.CharField(max_length=50)
+    Description = models.TextField()
+    DueDate = models.DateField()
+    Owner = models.ForeignKey(User, on_delete=models.CASCADE)
